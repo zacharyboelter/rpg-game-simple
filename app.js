61b6834e26ed6f2529628f4bdc43dbbbd126e9f9
Zachary Boelter <zboelter@gmail.com>
2022-03-20 14:34:25 -0500

    renderCharacter function with 5 arguments

--- app.js
+++ app.js
@@ -5,37 +5,27 @@ const heroAvatar = './images/wizard.png'
 const heroHealth = '60'
 const heroDiceRoll = 6
 
-const monsterElementId = 'orc'
+const monsterElementId = 'monster'
 const monsterName = 'Orc'
 const monsterAvatar = './images/orc.png'
 const monsterHealth = '10'
 const monsterDiceRoll = 4
 
 
-renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll) {
-    return
+function renderCharacter(elementId, name, avatar, health, diceRoll) {
+    document.getElementById(elementId).innerHTML = `
+    <div class="character-card">
+        <h4 class="name"> ${name} </h4>
+        <img class="avatar" src="${avatar}" />
+        <p class="health">health: <b> ${health} </b></p>
+        <div class="dice-container">
+            <div class="dice"> ${diceRoll} </div>
+        </div>
+    </div>
+`
 }
 
+renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll)
 
+renderCharacter(monsterElementId, monsterName, monsterAvatar, monsterHealth, monsterDiceRoll)
 
-
-document.querySelector('#hero').innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Wizard </h4>
-        <img class="avatar" src="./images/wizard.png" />
-        <p class="health">health: <b> 60 </b></p>
-        <div class="dice-container">
-            <div class="dice"> 6 </div>
-        </div>
-    </div>
-`
-document.querySelector('#monster').innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Orc </h4>
-        <img class="avatar" src="./images/orc.png" />
-        <p class="health">health: <b> 10 </b></p>
-        <div class="dice-container">
-            <div class="dice"> 4 </div>
-        </div>
-    </div>
-`
