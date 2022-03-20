1411a37887919f6c118dfed57fc0adf67e3243b7
Zachary Boelter <zboelter@gmail.com>
2022-03-20 16:08:17 -0500

    destructure objects and update template literal as such

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hero = {
     elementId: 'hero',
     name: 'Wizard',
     avatar: './images/wizard.png',
-    health: '60',
+    health: 60,
     diceRoll: 6
 }
 
@@ -11,19 +11,20 @@ const monster = {
     elementId: 'monster',
     name: 'Orc',
     avatar: './images/orc.png',
-    health: '10',
+    health: 10,
     diceRoll: 4
 }
 
 
 function renderCharacter(data) {
-    document.getElementById(data.elementId).innerHTML = `
+    const {elementId, name, avatar, health, diceRoll} = data
+    document.getElementById(elementId).innerHTML = `
     <div class="character-card">
-        <h4 class="name"> ${data.name} </h4>
-        <img class="avatar" src="${data.avatar}" />
-        <p class="health">health: <b> ${data.health} </b></p>
+        <h4 class="name"> ${name} </h4>
+        <img class="avatar" src="${avatar}" />
+        <p class="health">health: <b> ${health} </b></p>
         <div class="dice-container">
-            <div class="dice"> ${data.diceRoll} </div>
+            <div class="dice"> ${diceRoll} </div>
         </div>
     </div>
 `
