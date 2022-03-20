f061b6ef47309ef456dacdfc5db45c22e0663405
Zachary Boelter <zboelter@gmail.com>
2022-03-20 14:25:57 -0500

    strip out char data and store in variables

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 
+const heroElementId = 'hero'
+const heroName = 'Wizard'
+const heroAvatar = './images/wizard.png'
+const heroHealth = '60'
+const heroDiceRoll = 6
+
+const monsterElementId = 'orc'
+const monsterName = 'Orc'
+const monsterAvatar = './images/orc.png'
+const monsterHealth = '10'
+const monsterDiceRoll = 4
+
+
+renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDiceRoll) {
+    return
+}
+
+
+
 
 document.querySelector('#hero').innerHTML = `
     <div class="character-card">
