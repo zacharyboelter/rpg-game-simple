ec2309407f063b31dce71fe595f9b912142d716a
Zachary Boelter <zboelter@gmail.com>
2022-03-21 17:17:37 -0500

    done for day

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 import characterData from "./data.js"
-import {getDiceRollArray} from "./utils.js"
 import Character from "./Character.js"
-
 
 const wizard = new Character(characterData.hero)
 const villain = new Character(characterData.monster)
 
 function render() {
-    document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
-    document.getElementById(villain.elementId).innerHTML = villain.getCharacterHtml()
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = villain.getCharacterHtml()
 }
 
 render()
--- data.js
+++ data.js
@@ -1,20 +1,17 @@
 
 const characterData = {
     hero: {
-        elementId: 'hero',
         name: 'Merlin',
         avatar: './images/wizard.png',
         health: 60,
         diceCount: 3
     },
     monster: {
-        elementId: 'monster',
         name: 'Madam Mim',
         avatar: './images/madam-mim.png',
         health: 10,
         diceCount: 1
     }
 }
-
 
 export default characterData
