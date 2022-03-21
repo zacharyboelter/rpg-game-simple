cff67a5a0d4fe636cd17dcd106794cc6513cff18
Zachary Boelter <zboelter@gmail.com>
2022-03-21 11:19:59 -0500

    convert for loop in function to .map()

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const hero = {
     name: 'Wizard',
     avatar: './images/wizard.png',
     health: 60,
-    diceRoll: 6
+    diceRoll: [6, 1, 5],
+    diceCount: 3
 }
 
 const monster = {
@@ -12,19 +13,31 @@ const monster = {
     name: 'Orc',
     avatar: './images/orc.png',
     health: 10,
-    diceRoll: 4
+    diceRoll: [4],
+    diceCount: 1
 }
 
 
 function renderCharacter(data) {
-    const {elementId, name, avatar, health, diceRoll} = data
+    const {elementId, name, avatar, health, diceRoll, diceCount} = data
+
+    let diceHtml = diceRoll.map(function(dice) {
+        return `<div class="dice">${dice}</div>`
+    })
+
+    // for (let i = 0; i < diceCount; i ++) {
+    //     diceHtml += `<div class="dice">${diceRoll[i]}</div>`
+    // }
+
+    
+
     document.getElementById(elementId).innerHTML = `
     <div class="character-card">
         <h4 class="name"> ${name} </h4>
         <img class="avatar" src="${avatar}" />
         <p class="health">health: <b> ${health} </b></p>
         <div class="dice-container">
-            <div class="dice"> ${diceRoll} </div>
+            ${diceHtml}
         </div>
     </div>
 `
