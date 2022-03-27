9c05f31f7c38543572b5a546d849becbbb065b0a
Zachary Boelter <zboelter@gmail.com>
2022-03-27 13:55:40 -0500

    setTimeout and disable button during that interval

--- javascript/app.js
+++ javascript/app.js
@@ -9,7 +9,7 @@ const getnewMonster = () => {
 let monstersArray = ["mim", "crocodile", "dragon"];
 const wizard = new Character(characterData.hero)
 let monster = getnewMonster()
-
+let isWaiting = false
 
 
 function render() {
@@ -20,38 +20,49 @@ function render() {
 render()
 
 function attack() {
-    wizard.getDiceHtml()
-    monster.getDiceHtml()
-    wizard.takeDamage(monster.currentDiceScore)
-    monster.takeDamage(wizard.currentDiceScore)
-    render()
-    if (wizard.isDead) {
-        endGame()
-    } 
-    else if (monster.isDead) {
-        if (monstersArray.length > 0) {
-            monster = getnewMonster()
-            render()
-        } else {
+    if(!isWaiting){
+        wizard.getDiceHtml()
+        monster.getDiceHtml()
+        wizard.takeDamage(monster.currentDiceScore)
+        monster.takeDamage(wizard.currentDiceScore)
+        render()
+        if (wizard.isDead) {
             endGame()
+        } 
+        else if (monster.isDead) {
+            isWaiting = true
+            if (monstersArray.length > 0) {
+                setTimeout( () => {
+                    monster = getnewMonster()
+                    render()
+                    isWaiting= false
+                }, 1000)
+            } else {
+                endGame()
+            }
         }
-    }
+    }   
 }
 
 // ternary that states if both are dead, no victors. Wiz health more than 0, wiz won. else villain.
 function endGame() {
+    isWaiting = true
     const endMessage = wizard.health === 0 && monster.health === 0 ? 
     'No victors - all creatures are dead'
         : wizard.health > 0 ? `${wizard.name} has defeated ${monster.name}` 
         : `${monster.name} has defeated ${wizard.name}` 
         
-    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
-    document.body.innerHTML = 
-    `<div class="end-game">
-        <h2>Game Over</h2>
-        <h3>${endMessage}</h3>
-        <p class="end-emoji">${endEmoji}</p>
-    </div>` 
+        const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+        setTimeout(()=>{
+            document.body.innerHTML = `
+                <div class="end-game">
+                    <h2>Game Over</h2> 
+                    <h3>${endMessage}</h3>
+                    <p class="end-emoji">${endEmoji}</p>
+                </div>
+                `
+        }, 1500)
+    
 }
 
 document.getElementById('attack-button').addEventListener('click', attack)
