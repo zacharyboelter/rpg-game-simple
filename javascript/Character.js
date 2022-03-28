8482eaa6d68bfa3a56b3ecc1fb2d23eeeedd526b
Zachary Boelter <zboelter@gmail.com>
2022-03-28 12:49:38 -0500

    styles adjust

--- javascript/Character.js
+++ javascript/Character.js
@@ -49,61 +49,8 @@ class Character{
             </div>
         `}
     }
-    // Replace this.blah with an Object.assign() method
-    // Object.assign(target, source)
-    
-
-   
-    //maxHealth with always be the hardCoded health in data because the constructor 
-    //function is only called once and that is the value that will be stored in maxHealth
-    
-    // Move getDiceHtml function to method on the Character construction function
-    // function that takes diceCount, calls random from first function. Maps through array above and sets the html for each die to the random numbers.
-    // Update ~~~ sets currentDiceScore to whatever the diceRollArray puts out
-    
-
-    
-
-    //function that will handle the damage inflicted on each character
-    //use reduce method to get sum of diceRoll, store it in totalAttackScore 
-    
-
-    
 
 export default Character
 
 
 
-
-// const moduleStats = {
-//     module1: {
-//         moduleName: 'Learn JS',
-//         studentsEnrolled: 2340,
-//         studentsCompleted: 2210
-//     },
-//     module2: {
-//         moduleName: 'CSS Basics',
-//         studentsEnrolled: 1893,
-//         studentsCompleted: 1810
-//     },
-//     module3: {
-//         moduleName: 'Responsive Design',
-//         studentsEnrolled: 4600,
-//         studentsCompleted: 4357
-//     }
-// }
-// class Module {
-// //logic goes inside the constructor method as a property as THIS.____
-//     constructor(data){
-//         //assign this to data that is being passed as parameter
-//         Object.assign(this, data)
-//         this.percentCompletedModule = this.studentsCompleted / this.studentsEnrolled * 100
-//     }
-//     logPercentCompletedModule(){
-//         console.log(this.percentCompletedModule)
-//     }
-// }
-
-// //new instance of module
-// const responsiveDesign = new Module(moduleStats.module3)
-// responsiveDesign.logPercentCompletedModule()
